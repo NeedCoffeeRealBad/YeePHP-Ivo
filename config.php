2a78ad1a17d3b00bdd74526bfa222ef7a08ae052
Ivo Sem <thefuckisthat@abv.bg>
2017-03-15 11:39:13 +0200

    Second commit
    For some reason the first commit didn't upload all files.

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+    return array(
+                'templates.path'    => __DIR__ . '/templates/',
+                'debug' => true,
+                'log.enabled' => true,
+                'log.writer' => new \Yee\Log\FileLogger(
+                    array(
+                        'path' => __DIR__.'/logs',
+                        'name_format' => 'Y-m-d',
+                        'message_format' => '%label% - %date% - %message%'
+                    )
+                ),
+                'session' => 'php',   // php, database or memcached
+                'cache.path'=> __DIR__ . '/cache',
+                'cache.timeout'=> 1800,
+                   'database' => array(
+                'db1' => array(
+                                'database.host' => 'localhost',
+                                'database.name' => 'tourist_agency',
+                                'database.user' => 'root',
+                                'database.pass' => '',
+                                'database.port' => 3306
+                                )
+                            )
+                );
