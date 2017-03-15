076d5edbda85fc748e7090d8ef4ad03fb5d41a20
Ivo Sem <thefuckisthat@abv.bg>
2017-03-15 11:17:20 +0200

    First commit
    So far it's just a drop down menu with a GIF as a background. Updates
    are comming.

---
+++ App/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+use Yee\Yee;
+use Yee\Managers\RoutingCacheManager;
+use Yee\Managers\CacheManager;
+
+class HomeController extends \Yee\Managers\Controller\Controller
+{
+
+
+    /**
+     * @Route('/(home)')
+     * @Name('home.index')
+     */
+    public function index()
+    {
+        $app = $this->getYee();
+        $app->render('HomeFolder/home.twig');
+    }
+}
---
+++ cache/routing/dc0b16e06e8e26c05af84ea4823a5a26.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * Generated with RoutingCacheManager
+ *
+ * on 2017-03-15 08:27:04
+ */
+
+$app = Yee\Yee::getInstance();
+
+$app->map("/(home)", "HomeController::___index")->via("GET")->name("home.index");
+
